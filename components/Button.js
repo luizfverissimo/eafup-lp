a1e8eab35b55dd50ddac184e98ba315f22b64fb0
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2022-05-10 18:20:34 -0300

    chore: edit hero section

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,6 @@
 
 export function Button({href, children, isPrimary = true}) {
   return (
-    <a className={`font-bold text-lg text-theme-white cursor-pointer px-2 py-1 rounded-lg transition-all hover:brightness-110 ${isPrimary ? "bg-theme-green" : "bg-theme-black"}`} href={href}>{children}</a>
+    <a className={`font-bold text-lg text-theme-white cursor-pointer px-3 py-1 rounded-lg transition-all hover:brightness-110 ${isPrimary ? "bg-theme-green" : "bg-theme-black"}`} href={href}>{children}</a>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Button } from '../components/Button';
 import { LogoFullBlack } from '../components/LogoFullBlack';
 import { Navbar } from '../components/Navbar';
 import config from '../config/config.md'
@@ -12,13 +13,15 @@ export default function Home({title, eventDate, registerDate}) {
     </Head>
     <Navbar/>
 
-    <section className="flex w-full mx-auto max-w-7xl h-[624px] justify-center flex-col">
+    <img src="/hero.jpg" className="relative object-cover w-full opacity-30 -z-10" />
+    <section className="flex w-full mx-auto max-w-7xl h-[624px] justify-center flex-col gap-6 absolute">
       <LogoFullBlack />
-      <h2>{eventDate}</h2>
-      <h3>{registerDate}</h3>
+      <h2 className="font-sans text-3xl text-theme-black max-w-[300px] font-light">{eventDate}</h2>
+      <h3 className="text-2xl font-bold font-tans text-theme-green" >{registerDate}</h3>
 
-      <div>
-        
+      <div className="flex items-center gap-6">
+        <Button>Inscreva-se</Button>
+        <Button isPrimary={false}>Programação</Button>
       </div>
     </section>
 
